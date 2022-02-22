099c63cdb5ff2d0ef68b206250b1b8448005c351
J0nathan550 <ihudokormov@gmail.com>
2022-02-22 20:46:16 +0200

    Added some UI elements to projects, and in code some functions.

--- Notepad/Form1.cs
+++ Notepad/Form1.cs
@@ -7,14 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Notepad
 {
     public partial class Form1 : Form
     {
+        //Dialogs
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog savefileDialog;
+        private FontDialog fontDialog;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // create new file
+        private void NewFile()
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e) // new
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) // open
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) // save
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e) // save as
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) // exit
+        {
+
         }
     }
 }
